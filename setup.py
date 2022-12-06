dd7e6b1b65b0b3486c864bafd4ffe4ff5bf9a242
Marcuth <thiago.playgames15@gmail.com>
2022-12-06 15:58:53 +0000

    update lib on pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme_bf:
 
 setup(
     name="wcdeetlist",
-    version="0.0.0.3.6",
+    version="0.0.0.3.8",
     license="MIT License",
     author="Marcuth",
     long_description=readme_content,
