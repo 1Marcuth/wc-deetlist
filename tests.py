5f4ab8b1dc847efb31d65b51bed8706d6fe4b3f0
Marcuth <thiago.playgames15@gmail.com>
2022-12-27 14:32:17 +0000

    implementação do fetcher de game localization

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from wcdeetlist.tools import get_dragon_full_data
+from wcdeetlist.fetcher import GameLocalization
 import json
 
-data = get_dragon_full_data("https://deetlist.com/dragoncity/dragon/highcolony")
+# data = get_dragon_full_data("https://deetlist.com/dragoncity/dragon/highcolony")
 
-print(json.dumps(data, indent=4))
+# print(json.dumps(data, indent=4))
--- wcdeetlist/fetcher/game_localization.py
+++ wcdeetlist/fetcher/game_localization.py
@@ -1,14 +1,32 @@
 from pydantic import validate_arguments
+from pyfilter import FromList
+import requests
 
-from ...config import DC_LOCALIZATION_ENDPOINT
+from ..config import DC_LOCALIZATION_ENDPOINT
 
 class GameLocalization:
-    @validate_arguments()
-    def __init__(self, language: str):
+    @validate_arguments
+    def __init__(
+        self,
+        language:str
+    ) -> None:
         self.__language = language
 
-    def get_dict(self) -> dict:
-        pass
+    def get(self) -> list[dict] | None:
+        print("> [localization-fetcher] Coletando dados de localização para tradução de textos e nomes...")
 
-    def get(self) -> list[dict]:
-        pass
+        response = requests.get(DC_LOCALIZATION_ENDPOINT.format(self.__language))
+
+        print("> [localization-fetcher] Coleta concluída | Analizando resposta...")
+
+        if response.status_code == 200:
+            return response.json()
+
+        else:
+            raise Exception(f"> [localization-fetcher-error] Ocorreu um erro ao fazer a requisção no endpoint de localização.\nSTATUS_CODE: {response.status_code}")
+
+    def get_dict(self) -> dict | None:
+        localization = self.get()
+        localization_dict = FromList(localization).join_keys_of_child_dicts_in_a_new_dict()
+
+        return localization_dict
